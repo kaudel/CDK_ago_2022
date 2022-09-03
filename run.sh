5c8d644de564d2d7fa60f08eca60a14a9d26ea8c
root <kaudel@gmail.com>
2022-09-03 18:45:14 +0000

    modified run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+
 docker stop api-rest || true &&  docker rm api-rest || true
 
 
-
 #run container application
-docker run -itd --name api-rest- -p 3000:3000 kaudel/miappcdk-ago22:1.0
+docker run -itd --name api-rest -p 3000:3000 kaudel/miappcdk-ago22:1.0
